9af5a3be5b49a885c0e9563e0d5a6c8b5bbdfa67
Denpa_Ghost <az.kinosaki@gmail.com>
2020-03-06 19:49:13 +0900

    [add]dotenv

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,10 @@
+require('dotenv').config();
+const {NODE_ENV} = process.env;
+
 export default {
+    env: {
+        NODE_ENV
+    },
     buildModules: [
         ['@nuxt/typescript-build', {
             typeCheck: true
