25d918a9c9dd74befbdc8e942670ee1e30c42a07
Denpa_Ghost <az.kinosaki@gmail.com>
2020-03-06 19:11:10 +0900

    [add]jestセットアップ

---
+++ app/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    transform: {
+        '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+        '.*\\.(ts)$': '<rootDir>/node_modules/ts-jest',
+        '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    },
+    moduleFileExtensions: [
+        'ts',
+        'js',
+        'vue'
+    ],
+    moduleNameMapper: {
+        "^~/(.+)": "<rootDir>/$1",
+        "^@/(.+)": "<rootDir>/$1"
+    }
+};
