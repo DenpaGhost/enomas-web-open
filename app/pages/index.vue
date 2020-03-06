5729132bc53671111808066fcb0bc354ca467310
Denpa_Ghost <az.kinosaki@gmail.com>
2020-03-06 18:48:59 +0900

    [add]nuxt.tsセットアップ

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>helloWorld</h1>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'nuxt-property-decorator'
+
+    @Component
+    export default class Index extends Vue {
+        get helloWorld(): string {
+            return 'Hello world!'
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
